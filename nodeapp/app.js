b6cc1787b9015f1b7caf9d26b82b2be7c4274532
Ahmed mosaad <108762470+AHMEDMOSSAD29@users.noreply.github.com>
2023-11-23 15:31:24 +0200

    Update app.js

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -13,7 +13,7 @@ app.get('/', (req, res) => {
             font-family: Arial, sans-serif;
             text-align: center;
             margin-top: 100px;
-            background-image: url('https://miro.medium.com/v2/resize:fit:1100/format:webp/0*bV6AQdCcj_2w_3u4.png');
+            background-image: url('https://resources.github.com/assets/img/devops/social-card.png');
             background-size: cover;
           }
           h1 {
