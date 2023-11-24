a8a99299d814abc79d7ce9e6ffe6d2a654aeb516
Ahmed mosaad <108762470+AHMEDMOSSAD29@users.noreply.github.com>
2023-11-24 13:27:53 +0200

    Update security_group.tf

--- security_group.tf
+++ security_group.tf
@@ -44,10 +44,10 @@ resource "aws_security_group" "my_security_group" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-# for jenkins task port 5000 added
+# for jenkins task port 3000 added
   ingress {
-    from_port   = 5000
-    to_port     = 5000
+    from_port   = 3000
+    to_port     = 3000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
